f2dc30ad4f8665b052f151a31127357283afc107
Lincoln Lee <git@lincoln.hk>
2012-05-18 15:33:24 +0800

    Add initial project file
    Signed-off-by: Lincoln Lee <git@lincoln.hk>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Json::Application.config.secret_token = 'e2b49fda91d2d5cc8a8469342cae0caa3ee659b836536f96a07fc93074c09720b64e889095e17f16eb18ad2f4f1ea315c1976bc145a2a0a5e848c63dc03fab46'
