169e567080246117f98dcc16fada3fe72568bc27
Lincoln Lee <git@lincoln.hk>
2012-05-18 17:11:56 +0800

    Add pretty controller logic
    Signed-off-by: Lincoln Lee <git@lincoln.hk>

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -1,10 +1,15 @@
 class JsonController < ApplicationController
 
   def index
-
   end
 
   def pretty
-
+    unless params[:input].nil?
+      require 'json'
+      input = JSON.parse(params[:input])
+      render :text => JSON.pretty_generate(input)
+    else
+      render :nothing => true
+    end
   end
 end
